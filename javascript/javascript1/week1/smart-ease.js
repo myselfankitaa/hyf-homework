b0c2e2b2ee65c12c880aa57a02cc50ec42e7368a
myselfankitaa <ankitaa.mishra@gmail.com>
2024-04-26 11:15:09 +0200

    make the changes as per suggestions

--- javascript/javascript1/week1/smart-ease.js
+++ javascript/javascript1/week1/smart-ease.js
@@ -5,22 +5,18 @@
 const yearOfBirth = 1988;
 let yearFuture = 2027;
 const age = yearFuture - yearOfBirth;
-console.log("You will be " + age + " years old in " + yearFuture);
+console.log(`You will be  ${age} years old in ${yearFuture}`);
 
 //Goodboy-Oldboy (A dog age calculator)
-const dogYearOfBirth = 2020;
+let dogYearOfBirth = 2020;
 let dogYearFuture = 2045;
-const dogAge = dogYearFuture - dogYearOfBirth;
+let dogAge = dogYearFuture - dogYearOfBirth;
 let dogYear = dogAge * 7;
 let shouldShowResultInDogYears = true;
 if (shouldShowResultInDogYears) {
-  console.log(
-    "Your dog will be " + dogYear + " dog years old in " + dogYearFuture
-  );
+  console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
 } else {
-  console.log(
-    "Your dog will be " + dogAge + " human years old in " + dogYearFuture
-  );
+  console.log(`Your dog will be ${dogAge} human years old in ${dogYearFuture}`);
 }
 
 //Housey pricey (A house price estimator)
@@ -29,10 +25,11 @@ const peterHouseDepth = 10;
 const peterHouseheight = 10;
 const peterGardenSizeInM2 = 100;
 const peterHouseActualPrice = 2500000;
-let volumeInMeters = peterHouseheight * peterHouseDepth * peterHouseWidth;
-let housePrice = volumeInMeters * 2.5 * 1000 + peterGardenSizeInM2 * 300;
-console.log("Actual house Price is " + housePrice);
-if (housePrice > peterHouseActualPrice) {
+let peterVolumeInMeters = peterHouseheight * peterHouseDepth * peterHouseWidth;
+let peterHousePrice =
+  peterVolumeInMeters * 2.5 * 1000 + peterGardenSizeInM2 * 300;
+console.log("Actual house Price is " + peterHousePrice);
+if (peterHousePrice > peterHouseActualPrice) {
   console.log("Peter paid too little.");
 } else {
   console.log("Peter paid too much.");
@@ -43,10 +40,12 @@ const juliaHouseDepth = 11;
 const juliaHouseheight = 8;
 const juliaGardenSizeInM2 = 70;
 const juliaHouseActualPrice = 1000000;
-volumeInMeters = juliaHouseheight * juliaHouseDepth * juliaHouseWidth;
-housePrice = volumeInMeters * 2.5 * 1000 + juliaGardenSizeInM2 * 300;
-console.log("Actual house Price is " + housePrice);
-if (housePrice > juliaHouseActualPrice) {
+const juliaVolumeInMeters =
+  juliaHouseheight * juliaHouseDepth * juliaHouseWidth;
+let juliaHousePrice =
+  juliaVolumeInMeters * 2.5 * 1000 + juliaGardenSizeInM2 * 300;
+console.log("Actual house Price is " + juliaHousePrice);
+if (juliaHousePrice > juliaHouseActualPrice) {
   console.log("Julia paid too little.");
 } else {
   console.log("Julia paid too much.");
@@ -77,11 +76,11 @@ const secondWords = [
   "Momentum",
   "Accelerate",
 ];
-const firstRandomName = Math.floor(Math.random() * 10);
-const secondRandomName = Math.floor(Math.random() * 10);
+const firstRandomName = Math.floor(Math.random() * firstWords.length);
+const secondRandomName = Math.floor(Math.random() * secondWords.length);
 const startupName =
   firstWords[firstRandomName] + " " + secondWords[secondRandomName];
 const startupLength = startupName.length;
 console.log(
-  `The startup: \"${startupName}\" contains ${startupLength} characters.`
+  `The startup: ${startupName} contains ${startupLength} characters.`
 );
