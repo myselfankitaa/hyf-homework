d56e7ff54d57640db6eeec6ead288e040deae2eb
myselfankitaa <ankitaa.mishra@gmail.com>
2024-05-02 16:46:20 +0200

    event appication file

---
+++ javascript/javascript1/week2/event.js
@@ -0,0 +1,43 @@
+// console.log("inside event.");
+const now = new Date();
+let day = now.getDay();
+let newday;
+let calcweekday;
+
+function getDay(eventDay) {
+  const getremaindarWeekDay = eventDay % 7;
+  let weekday = day + getremaindarWeekDay;
+
+  if (weekday >= 7) {
+    calcweekday = weekday % 7;
+  } else {
+    calcweekday = weekday;
+  }
+
+  switch (calcweekday) {
+    case 0:
+      newday = "Sunday";
+      break;
+    case 1:
+      newday = "Monday";
+      break;
+    case 2:
+      newday = "Tuesday";
+      break;
+    case 3:
+      newday = "Wednesday";
+      break;
+    case 4:
+      newday = "Thursday";
+      break;
+    case 5:
+      newday = "Friday";
+      break;
+    case 6:
+      newday = "Saturday";
+      break;
+  }
+  console.log(`The event is held on ${newday}`);
+}
+
+getDay(99);
