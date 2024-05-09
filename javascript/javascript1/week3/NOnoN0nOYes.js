54fb77d2f02a523cd8c6fcf761e52919eeb1a80e
myselfankitaa <ankitaa.mishra@gmail.com>
2024-05-10 00:44:00 +0200

    Correction done

--- javascript/javascript1/week3/NOnoN0nOYes.js
+++ javascript/javascript1/week3/NOnoN0nOYes.js
@@ -1,31 +1,31 @@
 // Save a note
-let note = [];
+const notes = [];
 function saveNote(content, id) {
   const addNote = { content: content, id: id };
-  return note.push(addNote);
+  return notes.push(addNote);
 }
 saveNote("Pick up groceries", 1);
 saveNote("Do laundry", 2);
-console.log(note);
+console.log(notes);
 
 // get note
 function getNote(id) {
-  for (let i = 0; i < note.length; i++) {
-    if (note[i].id === id) {
-      return note[i];
-    } else if (typeof note[i].id !== Number) {
-      return `Error!! This is not a valid id`;
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id === id) {
+      return notes[i];
+      // } else if (typeof note[i].id !== Number) {
+      //   return `Error!! This is not a valid id`;
     } else {
       return `Error!! This is not a valid id`;
     }
   }
 }
-console.log(getNote(2));
+console.log(getNote(6));
 
 //Log Out Notes
 function logOutNotesFormatted() {
-  for (let i = 0; i < note.length; i++) {
-    const newNote = note[i];
+  for (let i = 0; i < notes.length; i++) {
+    const newNote = notes[i];
     console.log(
       `The note with id: ${newNote.id}, has the following note text: ${newNote.content}`
     );
@@ -37,12 +37,12 @@ logOutNotesFormatted();
 
 function editNote(id) {
   let noteFound = false;
-  for (let i = 0; i < note.length; i++) {
-    if (note[i].id === id) {
-      note[i].content = prompt("Edit your note");
-      console.log(note);
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id === id) {
+      notes[i].content = prompt("Edit your note");
+      console.log(notes);
       noteFound = true;
-      break;
+      return;
     }
   }
   if (!noteFound) {
