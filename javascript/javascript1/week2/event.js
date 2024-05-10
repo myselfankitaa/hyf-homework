79bb9713d7a01a900ac49b6f284cb43c2b79a03b
myselfankitaa <ankitaa.mishra@gmail.com>
2024-05-10 16:05:22 +0200

    chnage the switch method in to object

--- javascript/javascript1/week2/event.js
+++ javascript/javascript1/week2/event.js
@@ -1,8 +1,16 @@
 // console.log("inside event.");
-const now = new Date();
-let day = now.getDay();
-let newday;
+// const now = new Date();
+let day = new Date().getDay();
 let calcweekday;
+const weekList = {
+  0: "Sunday",
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+};
 
 function getDay(eventDay) {
   const getremaindarWeekDay = eventDay % 7;
@@ -13,31 +21,51 @@ function getDay(eventDay) {
   } else {
     calcweekday = weekday;
   }
-
-  switch (calcweekday) {
-    case 0:
-      newday = "Sunday";
-      break;
-    case 1:
-      newday = "Monday";
-      break;
-    case 2:
-      newday = "Tuesday";
-      break;
-    case 3:
-      newday = "Wednesday";
-      break;
-    case 4:
-      newday = "Thursday";
-      break;
-    case 5:
-      newday = "Friday";
-      break;
-    case 6:
-      newday = "Saturday";
-      break;
-  }
-  console.log(`The event is held on ${newday}`);
+  console.log(`The event is held on ${weekList[calcweekday]}`);
 }
 
-getDay(99);
+getDay(2);
+
+// // console.log("inside event.");
+// const now = new Date();
+// let day = now.getDay();
+// let newday;
+// let calcweekday;
+
+// function getDay(eventDay) {
+//   const getremaindarWeekDay = eventDay % 7;
+//   let weekday = day + getremaindarWeekDay;
+
+//   if (weekday >= 7) {
+//     calcweekday = weekday % 7;
+//   } else {
+//     calcweekday = weekday;
+//   }
+
+//   switch (calcweekday) {
+//     case 0:
+//       newday = "Sunday";
+//       break;
+//     case 1:
+//       newday = "Monday";
+//       break;
+//     case 2:
+//       newday = "Tuesday";
+//       break;
+//     case 3:
+//       newday = "Wednesday";
+//       break;
+//     case 4:
+//       newday = "Thursday";
+//       break;
+//     case 5:
+//       newday = "Friday";
+//       break;
+//     case 6:
+//       newday = "Saturday";
+//       break;
+//   }
+//   console.log(`The event is held on ${newday}`);
+// }
+
+// getDay(99);
