2912de94698b1557a9e88680119def9540a12570
myselfankitaa <ankitaa.mishra@gmail.com>
2024-08-16 14:59:27 +0200

    Apply try catch to all the requests

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -17,28 +17,33 @@ app.listen(port, () => {
 });
 
 app.get("/search", (req, res) => {
-  const query = req.query.q; // Convert query to lowercase
-  const jsonData = fs.readFileSync("./document.json");
-  const data = JSON.parse(jsonData);
+  try {
+    const query = req.query.q; // Convert query to lowercase
+    const jsonData = fs.readFileSync("./document.json");
+    const data = JSON.parse(jsonData);
 
-  if (!query) {
-    return res.send(data);
-  }
+    if (!query) {
+      return res.send(data);
+    }
 
-  let found = false;
+    let found = false;
 
-  const matchingData = data.filter(
-    (element) =>
-      String(element.value).toLowerCase().includes(query.toLowerCase()) ||
-      String(element.description).toLowerCase().includes(query.toLowerCase())
-  );
+    const matchingData = data.filter(
+      (element) =>
+        String(element.value).toLowerCase().includes(query.toLowerCase()) ||
+        String(element.description).toLowerCase().includes(query.toLowerCase())
+    );
 
-  if (matchingData.length > 0) {
-    res.send(matchingData);
-    found = true;
-  }
-  if (!found) {
-    res.status(404).send("There is an error in fetching data");
+    if (matchingData.length > 0) {
+      res.send(matchingData);
+      found = true;
+    }
+    if (!found) {
+      res.status(404).send("Data does not exist");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "There is an error in fetching data" });
   }
 });
 
@@ -57,32 +62,37 @@ app.get("/document/:id", (req, res) => {
       res.status(404).send(`${param} does not match the Data`);
     }
   } catch (err) {
-    res.status(404).send(err, `Error in parsing and reading the file`);
+    res.status(500).send(err, `Error in parsing and reading the file`);
   }
 });
 
 app.post("/search", (req, res) => {
-  const query = req.query.q;
-  const fields = req.body.fields;
+  try {
+    const query = req.query.q;
+    const fields = req.body.fields;
 
-  if (query && fields) {
-    return res
-      .status(400)
-      .send("Both query parameter and fields in body cannot be provided");
-  }
+    if (query && fields) {
+      return res
+        .status(400)
+        .send("Both query parameter and fields in body cannot be provided");
+    }
 
-  const jsonData = fs.readFileSync(path.resolve("./document.json"));
-  const data = JSON.parse(jsonData);
+    const jsonData = fs.readFileSync(path.resolve("./document.json"));
+    const data = JSON.parse(jsonData);
 
-  const results = data.filter((item) => {
-    return Object.keys(fields).every((key) => {
-      return item[key] === fields[key];
+    const results = data.filter((item) => {
+      return Object.keys(fields).every((key) => {
+        return item[key] === fields[key];
+      });
     });
-  });
 
-  if (results.length > 0) {
-    res.json(results);
-  } else {
-    res.status(404).send("No matching data found");
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.status(404).send("No matching data found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Request could not completed" });
   }
 });
